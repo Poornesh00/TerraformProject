19a8c84dfdcc378ee9d52caf55f9f6c1a6118701
IamPoornesh <95753331+Poornesh00@users.noreply.github.com>
2026-04-14 18:20:20 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "poornesh-terraform-state-2026"
-    key            = "default.tfstate"
+    key            = "default.tfstate"   # placeholder
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
