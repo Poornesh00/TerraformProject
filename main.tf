1e1783916928aa1035e6ec484446090e8533e9e1
IamPoornesh <95753331+Poornesh00@users.noreply.github.com>
2026-04-14 15:49:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -38,6 +38,8 @@ resource "aws_instance" "ec2" {
   ami           = data.aws_ami.amazon_linux.id
   instance_type = "t2.micro"
 
+key_name = "aws-login"
+
   tags = {
     Name        = var.name
     Environment = var.env
