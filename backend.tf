542f8afa7ae2943cc874bd675d120501ea246234
IamPoornesh <95753331+Poornesh00@users.noreply.github.com>
2026-04-14 14:59:16 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "YOUR_UNIQUE_NAME"
-    key            = "dev/terraform.tfstate"
+    bucket         = "YOUR_BUCKET_NAME"
+    key            = "terraform/self-service.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
