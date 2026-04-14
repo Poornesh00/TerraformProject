1a06bd2e1547d466792633f746c6ee0a9dfaedf0
IamPoornesh <95753331+Poornesh00@users.noreply.github.com>
2026-04-14 15:09:04 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "resource_type" {
   description = "Type of resource to create"
   type        = string
 }
+
+variable "env" {
+  description = "Environment (dev/prod)"
+  type        = string
+}
